1351b01ebd85714eda7fafd2ed66db81e9d0a0c5
FuriousGamer414 <110168231+FuriousGamer414@users.noreply.github.com>
2024-07-22 17:19:34 +0530

    Update misc.js

--- plugins/misc.js
+++ plugins/misc.js
@@ -1,8 +1,7 @@
 /**
 * @project_name Queen Amdi [WA Multi-device]
-* @author BlackAmda <https://github.com/BlackAmda>
+* @author Debraj Chanda
 * @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
-* @link <https://github.com/BlackAmda/QueenAmdi>
 * @version 4.0.7
 * @file  misc.js - QueenAmdi miscellaneous commands
 
